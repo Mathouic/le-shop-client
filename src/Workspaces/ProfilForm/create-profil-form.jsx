d6fc7805f3ecfbf43e4dc23e7742970d51bb0845
mathias.laguerie@bertger-levrault.com <mathias.laguerie@laposte.net>
2023-03-23 18:14:19 +0100

    Bind update, delete profil to the services

--- src/Workspaces/ProfilForm/create-profil-form.jsx
+++ src/Workspaces/ProfilForm/create-profil-form.jsx
@@ -1,4 +1,4 @@
-import {Form as BootForm, Button}  from 'react-bootstrap'
+import {Form as BootForm, Button, Container, Row, Col}  from 'react-bootstrap'
 import {Form, redirect} from 'react-router-dom'
 import {profilStore} from '../../Store/ProfilStore/profil-store';
 
@@ -8,23 +8,44 @@ export async function action({ request, params }){
     console.log(request);
     const formData = await request.formData();
     const updates = Object.fromEntries(formData);
-    profilStore.POST({mailRef: updates.email, pwdRef: "coucou"})
+    profilStore.POST({mailRef: updates.email, pwdRef: updates.password})
     return redirect("/home")
 }
 
 export default function CreateProfilForm() {
     return (
+        
         <Form method='post'  >
-           <BootForm.Group className="mb-12" controlId="formBasicEmail">
-                <BootForm.Label>Email address</BootForm.Label>
-                <BootForm.Control type="email" placeholder="Enter email" name="email"/>
-                <BootForm.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </BootForm.Text>
-            </BootForm.Group>
-            <Button  type="submit">
-                Submit
-            </Button>
+            <Container>
+                <Row>
+                    <Col  xs={2}>
+                        <BootForm.Label>Email address</BootForm.Label>
+                    </Col>
+                    <Col xs={6}>
+                        <BootForm.Control type="email" placeholder="Enter email" name="email"/>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col  xs={2}>
+                        <BootForm.Label>Password</BootForm.Label>
+                    </Col>
+                    <Col xs={6}>
+                        <BootForm.Control type="password" placeholder="Enter password" name="password"/>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col xs={2}>
+                    <Button  type="submit">
+                        Submit
+                    </Button>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+           
+            
         </Form>
     )
 }
